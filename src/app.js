e55c30ddc697bddf710f09bb65232c3fff4694ef
Mateus Panzarini <panza@Panza13.local>
2019-09-08 20:09:57 -0300

    included sentry io and youch dev

--- src/app.js
+++ src/app.js
@@ -2,19 +2,28 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
-
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -24,6 +33,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (error, req, resp, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(error, req).toJSON();
+
+        return resp.status(500).json(errors);
+      }
+
+      return resp.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
